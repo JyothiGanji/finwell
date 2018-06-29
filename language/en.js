3325368a4af756f35db1cbf27cc60eb3a8fae1ab
JyothiGanji <38552339+JyothiGanji@users.noreply.github.com>
2018-06-29 10:55:49 +0530

    width changed

--- language/en.js
+++ language/en.js
@@ -108,7 +108,7 @@ var Translations = {
      */
     "ChapterSettings": [
         {
-            "label": "INTRODUCTION",
+            "label": "Introduction",
             "states": [
                 {
                     "cardId": "START",
@@ -117,7 +117,7 @@ var Translations = {
             ]
         },
         {
-            "label": "WHAT'S IT IN FOR YOU",
+            "label": "What's it in for you",
             "states": [
                 {
                     "cardId": "6f3f4533",
@@ -130,11 +130,11 @@ var Translations = {
             ]
         },
         {
-            "label": "PRODUCT INFO",
+            "label": "Product Info",
             "states": [
                 {
                     "cardId": "42bca2b4",
-                    "width": 10
+                    "width": 15
                 }
                 
             ]
