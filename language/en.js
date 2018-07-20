05e7fc92ef7f3c6766cb7e2b9e49d119418a2069
JyothiGanji <38552339+JyothiGanji@users.noreply.github.com>
2018-07-20 10:40:25 +0530

    Update en.js

--- language/en.js
+++ language/en.js
@@ -98,7 +98,9 @@ var Translations = {
             "trackingName": "promo2",
             "url": "http://example.com/",
             "label": "Promo (Below)",
-            "height": "25%",
+            /**"height": "25%",**/
+             width: "118px",
+             height: "40px",
             "desktopImage": "./images/promo-buy-now-btn.png",
             "mobileImage": "./images/promo_lanedscape.png"
         }
